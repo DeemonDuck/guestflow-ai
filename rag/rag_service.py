8966fe1791ca508a774555757c0a2a845f5a5560
Ridham Taneja <ridham06042003@gmail.com>
2026-05-02 12:58:46 +0530

    Improved FAQ retrieval scoring for better query matching

--- rag/rag_service.py
+++ rag/rag_service.py
@@ -1,16 +1,23 @@
+import re
+
+
+def clean_text(text):
+    return re.findall(r'\w+', text.lower())
+
+
 def retrieve_faq(query):
 
-    with open("rag/hotel_FAQ.txt", "r") as file:
+    with open("rag/hotel_faq.txt", "r") as file:
         faq_data = file.readlines()
 
-    query_words = set(query.lower().split())
+    query_words = set(clean_text(query))
 
     best_match = None
     highest_score = 0
 
     for line in faq_data:
 
-        line_words = set(line.lower().split())
+        line_words = set(clean_text(line))
 
         score = len(query_words.intersection(line_words))
 
